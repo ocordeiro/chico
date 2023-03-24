2253d18aad6acc8fb96069307b8d6a64b0fdd2b6
ocordeiro <me@cordeiro.me>
2023-03-24 18:43:48 -0500

    splitter

--- utils.cpp
+++ utils.cpp
@@ -3,32 +3,55 @@
 #include <fstream>
 #include <regex>
 
-bool gpt_params_parse(int argc, char ** argv, gpt_params & params) {
-    for (int i = 1; i < argc; i++) {
+bool gpt_params_parse(int argc, char **argv, gpt_params &params)
+{
+    for (int i = 1; i < argc; i++)
+    {
         std::string arg = argv[i];
 
-        if (arg == "-s" || arg == "--seed") {
+        if (arg == "-s" || arg == "--seed")
+        {
             params.seed = std::stoi(argv[++i]);
-        } else if (arg == "-t" || arg == "--threads") {
+        }
+        else if (arg == "-t" || arg == "--threads")
+        {
             params.n_threads = std::stoi(argv[++i]);
-        } else if (arg == "-p" || arg == "--prompt") {
+        }
+        else if (arg == "-p" || arg == "--prompt")
+        {
             params.prompt = argv[++i];
-        } else if (arg == "-n" || arg == "--n_predict") {
+        }
+        else if (arg == "-n" || arg == "--n_predict")
+        {
             params.n_predict = std::stoi(argv[++i]);
-        } else if (arg == "--top_k") {
+        }
+        else if (arg == "--top_k")
+        {
             params.top_k = std::stoi(argv[++i]);
-        } else if (arg == "--top_p") {
+        }
+        else if (arg == "--top_p")
+        {
             params.top_p = std::stof(argv[++i]);
-        } else if (arg == "--temp") {
+        }
+        else if (arg == "--temp")
+        {
             params.temp = std::stof(argv[++i]);
-        } else if (arg == "-b" || arg == "--batch_size") {
+        }
+        else if (arg == "-b" || arg == "--batch_size")
+        {
             params.n_batch = std::stoi(argv[++i]);
-        } else if (arg == "-m" || arg == "--model") {
+        }
+        else if (arg == "-m" || arg == "--model")
+        {
             params.model = argv[++i];
-        } else if (arg == "-h" || arg == "--help") {
+        }
+        else if (arg == "-h" || arg == "--help")
+        {
             gpt_print_usage(argc, argv, params);
             exit(0);
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "error: unknown argument: %s\n", arg.c_str());
             gpt_print_usage(argc, argv, params);
             exit(0);
@@ -38,7 +61,8 @@ bool gpt_params_parse(int argc, char ** argv, gpt_params & params) {
     return true;
 }
 
-void gpt_print_usage(int argc, char ** argv, const gpt_params & params) {
+void gpt_print_usage(int argc, char **argv, const gpt_params &params)
+{
     fprintf(stderr, "usage: %s [options]\n", argv[0]);
     fprintf(stderr, "\n");
     fprintf(stderr, "options:\n");
@@ -57,96 +81,127 @@ void gpt_print_usage(int argc, char ** argv, const gpt_params & params) {
     fprintf(stderr, "\n");
 }
 
-void replace(std::string & str, const std::string & needle, const std::string & replacement) {
+void replace(std::string &str, const std::string &needle, const std::string &replacement)
+{
     size_t pos = 0;
-    while ((pos = str.find(needle, pos)) != std::string::npos) {
+    while ((pos = str.find(needle, pos)) != std::string::npos)
+    {
         str.replace(pos, needle.length(), replacement);
         pos += replacement.length();
     }
 }
 
-std::map<std::string, int32_t> json_parse(const std::string & fname) {
+std::map<std::string, int32_t> json_parse(const std::string &fname)
+{
     std::map<std::string, int32_t> result;
 
     // read file into string
     std::string json;
     {
         std::ifstream ifs(fname);
-        if (!ifs) {
+        if (!ifs)
+        {
             fprintf(stderr, "Failed to open %s\n", fname.c_str());
             exit(1);
         }
 
         json = std::string((std::istreambuf_iterator<char>(ifs)),
-                (std::istreambuf_iterator<char>()));
+                           (std::istreambuf_iterator<char>()));
     }
 
-    if (json[0] != '{') {
+    if (json[0] != '{')
+    {
         return result;
     }
 
     // parse json
     {
-        bool has_key  = false;
+        bool has_key = false;
         bool in_token = false;
 
         std::string str_key = "";
         std::string str_val = "";
 
         int n = json.size();
-        for (int i = 1; i < n; ++i) {
-            if (!in_token) {
-                if (json[i] == ' ') continue;
-                if (json[i] == '"') {
+        for (int i = 1; i < n; ++i)
+        {
+            if (!in_token)
+            {
+                if (json[i] == ' ')
+                    continue;
+                if (json[i] == '"')
+                {
                     in_token = true;
                     continue;
                 }
-            } else {
-                if (json[i] == '\\' && i+1 < n) {
-                    if (has_key == false) {
+            }
+            else
+            {
+                if (json[i] == '\\' && i + 1 < n)
+                {
+                    if (has_key == false)
+                    {
                         str_key += json[i];
-                    } else {
+                    }
+                    else
+                    {
                         str_val += json[i];
                     }
                     ++i;
-                } else if (json[i] == '"') {
-                    if (has_key == false) {
+                }
+                else if (json[i] == '"')
+                {
+                    if (has_key == false)
+                    {
                         has_key = true;
                         ++i;
-                        while (json[i] == ' ') ++i;
+                        while (json[i] == ' ')
+                            ++i;
                         ++i; // :
-                        while (json[i] == ' ') ++i;
-                        if (json[i] != '\"') {
-                            while (json[i] != ',' && json[i] != '}') {
+                        while (json[i] == ' ')
+                            ++i;
+                        if (json[i] != '\"')
+                        {
+                            while (json[i] != ',' && json[i] != '}')
+                            {
                                 str_val += json[i++];
                             }
                             has_key = false;
-                        } else {
+                        }
+                        else
+                        {
                             in_token = true;
                             continue;
                         }
-                    } else {
+                    }
+                    else
+                    {
                         has_key = false;
                     }
 
-                    ::replace(str_key, "\\u0120", " " ); // \u0120 -> space
+                    ::replace(str_key, "\\u0120", " ");  // \u0120 -> space
                     ::replace(str_key, "\\u010a", "\n"); // \u010a -> new line
-                    ::replace(str_key, "\\\"",    "\""); // \\\"   -> "
+                    ::replace(str_key, "\\\"", "\"");    // \\\"   -> "
 
-                    try {
+                    try
+                    {
                         result[str_key] = std::stoi(str_val);
-                    } catch (...) {
-                        //fprintf(stderr, "%s: ignoring key '%s' with value '%s'\n", fname.c_str(), str_key.c_str(), str_val.c_str());
-
+                    }
+                    catch (...)
+                    {
+                        // fprintf(stderr, "%s: ignoring key '%s' with value '%s'\n", fname.c_str(), str_key.c_str(), str_val.c_str());
                     }
                     str_key = "";
                     str_val = "";
                     in_token = false;
                     continue;
                 }
-                if (has_key == false) {
+                if (has_key == false)
+                {
                     str_key += json[i];
-                } else {
+                }
+                else
+                {
                     str_val += json[i];
                 }
             }
@@ -156,7 +211,8 @@ std::map<std::string, int32_t> json_parse(const std::string & fname) {
     return result;
 }
 
-std::vector<gpt_vocab::id> gpt_tokenize(const gpt_vocab & vocab, const std::string & text) {
+std::vector<gpt_vocab::id> gpt_tokenize(const gpt_vocab &vocab, const std::string &text)
+{
     std::vector<std::string> words;
 
     // first split the text into words
@@ -167,8 +223,10 @@ std::vector<gpt_vocab::id> gpt_tokenize(const gpt_vocab & vocab, const std::stri
         std::regex re(pat);
         std::smatch m;
 
-        while (std::regex_search(str, m, re)) {
-            for (auto x : m) {
+        while (std::regex_search(str, m, re))
+        {
+            for (auto x : m)
+            {
                 words.push_back(x);
             }
             str = m.suffix();
@@ -177,30 +235,40 @@ std::vector<gpt_vocab::id> gpt_tokenize(const gpt_vocab & vocab, const std::stri
 
     // find the longest tokens that form the words:
     std::vector<gpt_vocab::id> tokens;
-    for (const auto & word : words) {
-        if (word.size() == 0) continue;
+    for (const auto &word : words)
+    {
+        if (word.size() == 0)
+            continue;
 
         int i = 0;
         int n = word.size();
-        while (i < n) {
+        while (i < n)
+        {
             int j = n;
-            while (j > i) {
-                auto it = vocab.token_to_id.find(word.substr(i, j-i));
-                if (it != vocab.token_to_id.end()) {
+            while (j > i)
+            {
+                auto it = vocab.token_to_id.find(word.substr(i, j - i));
+                if (it != vocab.token_to_id.end())
+                {
                     tokens.push_back(it->second);
                     i = j;
                     break;
                 }
                 --j;
             }
-            if (i == n) {
+            if (i == n)
+            {
                 break;
             }
-            if (j == i) {
+            if (j == i)
+            {
                 auto sub = word.substr(i, 1);
-                if (vocab.token_to_id.find(sub) != vocab.token_to_id.end()) {
+                if (vocab.token_to_id.find(sub) != vocab.token_to_id.end())
+                {
                     tokens.push_back(vocab.token_to_id.at(sub));
-                } else {
+                }
+                else
+                {
                     fprintf(stderr, "%s: unknown token '%s'\n", __func__, sub.data());
                 }
                 ++i;
@@ -211,19 +279,21 @@ std::vector<gpt_vocab::id> gpt_tokenize(const gpt_vocab & vocab, const std::stri
     return tokens;
 }
 
-bool gpt_vocab_init(const std::string & fname, gpt_vocab & vocab) {
+bool gpt_vocab_init(const std::string &fname, gpt_vocab &vocab)
+{
     printf("%s: loading vocab from '%s'\n", __func__, fname.c_str());
 
     vocab.token_to_id = ::json_parse(fname);
 
-    for (const auto & kv : vocab.token_to_id) {
+    for (const auto &kv : vocab.token_to_id)
+    {
         vocab.id_to_token[kv.second] = kv.first;
     }
 
-    printf("%s: vocab size = %d\n", __func__, (int) vocab.token_to_id.size());
+    printf("%s: vocab size = %d\n", __func__, (int)vocab.token_to_id.size());
 
     // print the vocabulary
-    //for (auto kv : vocab.token_to_id) {
+    // for (auto kv : vocab.token_to_id) {
     //    printf("'%s' -> %d\n", kv.first.data(), kv.second);
     //}
 
@@ -231,36 +301,40 @@ bool gpt_vocab_init(const std::string & fname, gpt_vocab & vocab) {
 }
 
 gpt_vocab::id gpt_sample_top_k_top_p(
-        const gpt_vocab & vocab,
-        const float * logits,
-        int    top_k,
-        double top_p,
-        double temp,
-        std::mt19937 & rng) {
+    const gpt_vocab &vocab,
+    const float *logits,
+    int top_k,
+    double top_p,
+    double temp,
+    std::mt19937 &rng)
+{
     int n_logits = vocab.id_to_token.size();
 
     std::vector<std::pair<double, gpt_vocab::id>> logits_id;
     logits_id.reserve(n_logits);
 
     {
-        const double scale = 1.0/temp;
-        for (int i = 0; i < n_logits; ++i) {
-            logits_id.push_back(std::make_pair(logits[i]*scale, i));
+        const double scale = 1.0 / temp;
+        for (int i = 0; i < n_logits; ++i)
+        {
+            logits_id.push_back(std::make_pair(logits[i] * scale, i));
         }
     }
 
     // find the top K tokens
     std::partial_sort(
-            logits_id.begin(),
-            logits_id.begin() + top_k, logits_id.end(),
-            [](const std::pair<double, gpt_vocab::id> & a, const std::pair<double, gpt_vocab::id> & b) {
-        return a.first > b.first;
-    });
+        logits_id.begin(),
+        logits_id.begin() + top_k, logits_id.end(),
+        [](const std::pair<double, gpt_vocab::id> &a, const std::pair<double, gpt_vocab::id> &b)
+        {
+            return a.first > b.first;
+        });
 
     logits_id.resize(top_k);
 
     double maxl = -INFINITY;
-    for (const auto & kv : logits_id) {
+    for (const auto &kv : logits_id)
+    {
         maxl = std::max(maxl, kv.first);
     }
 
@@ -269,22 +343,27 @@ gpt_vocab::id gpt_sample_top_k_top_p(
     probs.reserve(logits_id.size());
 
     double sum = 0.0;
-    for (const auto & kv : logits_id) {
+    for (const auto &kv : logits_id)
+    {
         double p = exp(kv.first - maxl);
         probs.push_back(p);
         sum += p;
     }
 
     // normalize the probs
-    for (auto & p : probs) {
+    for (auto &p : probs)
+    {
         p /= sum;
     }
 
-    if (top_p < 1.0f) {
+    if (top_p < 1.0f)
+    {
         double cumsum = 0.0f;
-        for (int i = 0; i < top_k; i++) {
+        for (int i = 0; i < top_k; i++)
+        {
             cumsum += probs[i];
-            if (cumsum >= top_p) {
+            if (cumsum >= top_p)
+            {
                 top_k = i + 1;
                 probs.resize(top_k);
                 logits_id.resize(top_k);
@@ -292,17 +371,18 @@ gpt_vocab::id gpt_sample_top_k_top_p(
             }
         }
 
-        cumsum = 1.0/cumsum;
-        for (int i = 0; i < (int) probs.size(); i++) {
+        cumsum = 1.0 / cumsum;
+        for (int i = 0; i < (int)probs.size(); i++)
+        {
             probs[i] *= cumsum;
         }
     }
 
-    //printf("\n");
-    //for (int i = 0; i < (int) probs.size(); i++) {
-    //    printf("%d: '%s' %f\n", i, vocab.id_to_token.at(logits_id[i].second).c_str(), probs[i]);
-    //}
-    //exit(0);
+    // printf("\n");
+    // for (int i = 0; i < (int) probs.size(); i++) {
+    //     printf("%d: '%s' %f\n", i, vocab.id_to_token.at(logits_id[i].second).c_str(), probs[i]);
+    // }
+    // exit(0);
 
     std::discrete_distribution<> dist(probs.begin(), probs.end());
     int idx = dist(rng);
@@ -310,31 +390,50 @@ gpt_vocab::id gpt_sample_top_k_top_p(
     return logits_id[idx].second;
 }
 
-std::vector<std::vector<gpt_vocab::id>> split_sentences(const std::vector<gpt_vocab::id>& tokens, const gpt_vocab& vocab) {
+std::vector<std::vector<gpt_vocab::id>> split_sentences(const std::vector<gpt_vocab::id> &tokens, const gpt_vocab &vocab)
+{
     const gpt_vocab::id period_token = gpt_tokenize(vocab, ".")[0];
     const gpt_vocab::id question_token = gpt_tokenize(vocab, "?")[0];
     const gpt_vocab::id exclamation_token = gpt_tokenize(vocab, "!")[0];
-    const int max_tokens = 100;
+    const int max_tokens = 1024;
 
     std::vector<std::vector<gpt_vocab::id>> result;
     std::vector<gpt_vocab::id> current_sentence;
 
-    for (const auto& token : tokens) {
+    for (auto token_iter = tokens.begin(); token_iter != tokens.end(); ++token_iter)
+    {
+        const auto &token = *token_iter;
         current_sentence.push_back(token);
 
-        if (current_sentence.size() == max_tokens) {
-            result.push_back(current_sentence);
-            current_sentence.clear();
-        } else if (token == period_token || token == question_token || token == exclamation_token) {
-            result.push_back(current_sentence);
-            current_sentence.clear();
+        if (token == period_token || token == question_token || token == exclamation_token)
+        {
+            auto next_token_iter = std::next(token_iter);
+            if (next_token_iter != tokens.end())
+            {
+                size_t next_sentence_length = 0;
+                for (auto iter = next_token_iter; iter != tokens.end() && (*iter != period_token && *iter != question_token && *iter != exclamation_token); ++iter)
+                {
+                    ++next_sentence_length;
+                }
+
+                if (current_sentence.size() + next_sentence_length > max_tokens)
+                {
+                    result.push_back(current_sentence);
+                    current_sentence.clear();
+                }
+            }
+            else
+            {
+                result.push_back(current_sentence);
+                current_sentence.clear();
+            }
         }
     }
 
-    if (!current_sentence.empty()) {
+    if (!current_sentence.empty())
+    {
         result.push_back(current_sentence);
     }
 
     return result;
 }
-
